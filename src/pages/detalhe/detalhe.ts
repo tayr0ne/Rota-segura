c0d1bf548dd217f69a52046d12703a93c0df5182
SistemaGlobal <tayronewesleymartins@gmail.com>
2018-09-04 17:44:19 -0300

    Em desenvolvimento

---
+++ src/pages/detalhe/detalhe.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the DetalhePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-detalhe',
+  templateUrl: 'detalhe.html',
+})
+export class DetalhePage {
+  items: any;
+ 
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.items = this.navParams.get('item');
+  }
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public items:any;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public HttpClient: HttpClient){
+    this.loadData();
   }
 
+  loadData(){
+    let data:Observable<any>;
+    data = this.HttpClient.get('https://laravel-web-service.herokuapp.com/api/user');
+    data.subscribe(result =>{
+      this.items =result;
+    });  
+  }
+
+  openDetails(item) {
+    this.navCtrl.push('DetalhePage', {item: item});
+  }
+  
 }
